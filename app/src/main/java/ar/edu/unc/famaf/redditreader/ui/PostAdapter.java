e3a9b2301c1d4d4c5eb55064637342f0a426cc9e
Pedro Ignacio Nieto <pedronieto.cac@gmail.com>
2016-10-09 16:23:10 -0300

    Implemento ViewHolder

--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
@@ -37,33 +37,47 @@ public class PostAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
+        ViewHolder viewHolder = null;
         if(convertView == null){
             LayoutInflater inflater = (LayoutInflater) getContext().
                     getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.item_post, null);
+            viewHolder = new ViewHolder((ImageView) convertView.findViewById(R.id.postImageView),
+                                        (TextView) convertView.findViewById(R.id.postCategoriesTextView),
+                                        (TextView) convertView.findViewById(R.id.postDateTextView),
+                                        (TextView) convertView.findViewById(R.id.postContentTextView),
+                                        (TextView) convertView.findViewById(R.id.postCommentsCountTextView));
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
         PostModel postModel = postLst.get(position);
         if (postModel != null){
-            ImageView postImagePreview = (ImageView) convertView.findViewById(R.id.postImageView);
-//            TextView postCategory = (TextView) convertView.findViewById(R.id.postCategoriesTextView);
-            TextView postTitle = (TextView) convertView.findViewById(R.id.postCategoriesTextView); //TODO Asegurar que sea correcto
-            TextView postDate = (TextView) convertView.findViewById(R.id.postDateTextView);
-//            TextView postContent = (TextView) convertView.findViewById(R.id.postContentTextView);
-            TextView postAuthor = (TextView) convertView.findViewById(R.id.postContentTextView);
-            TextView postCommentsCount = (TextView) convertView.findViewById(R.id.postCommentsCountTextView);
-            /*TODO title --- category?? cambiar donde corresponda Chequear que es cada campo */
-
-            postImagePreview.setImageResource(postModel.getPostImageId());
-            postTitle.setText(postModel.getPostTitle());
-//            postDate.setText(Resources.getSystem().getString(R.string.time_of_post, postModel.getPostDate()));
+            viewHolder.postImageView.setImageResource(postModel.getPostImageId());
+            viewHolder.postCategoryTextView.setText(postModel.getPostTitle());
             String time = getContext().getString(R.string.time_of_post, postModel.getPostDate());
-            postDate.setText(time);
-            postAuthor.setText(postModel.getPostAuthor());
+            viewHolder.postDateTextView.setText(time);
+            viewHolder.postContentTextView.setText(postModel.getPostAuthor());
             String commentsCount = getContext().getString(R.string.comments_amounts, postModel.getPostCommentCount());
-//            postCommentsCount.setText(postModel.getPostCommentCount());
-
-            postCommentsCount.setText(commentsCount);
+            viewHolder.postCommentsCountTextView.setText(commentsCount);
+            /*TODO title --- category?? cambiar donde corresponda Chequear que es cada campo */
         }
         return convertView;
+    }
+    private class ViewHolder {
+        public final ImageView postImageView;
+        public final TextView postCategoryTextView;
+        public final TextView postDateTextView;
+        public final TextView postContentTextView;
+        public final TextView postCommentsCountTextView;
+        public ViewHolder(ImageView postImageView, TextView postCategoryTextView,
+                          TextView postDateTextView, TextView postContentTextView,
+                          TextView postCommentsCountTextView){
+            this.postImageView = postImageView;
+            this.postCategoryTextView = postCategoryTextView;
+            this.postDateTextView = postDateTextView;
+        cd An    this.postContentTextView = postContentTextView;
+            this.postCommentsCountTextView = postCommentsCountTextView;
+        }
     }
 }
