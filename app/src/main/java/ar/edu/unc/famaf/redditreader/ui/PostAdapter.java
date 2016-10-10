883e21934c61676ce568246344184574edda741b
Pedro Ignacio Nieto <pedronieto.cac@gmail.com>
2016-10-10 19:12:46 -0300

    Arreglo ids y borro post de mas

--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
@@ -1,12 +1,10 @@
 package ar.edu.unc.famaf.redditreader.ui;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,9 +13,7 @@ import java.util.List;
 import ar.edu.unc.famaf.redditreader.R;
 import ar.edu.unc.famaf.redditreader.model.PostModel;
 
-/**
- * Created by pedro on 06/10/16.
- */
+
 
 public class PostAdapter extends ArrayAdapter {
     private List<PostModel> postLst;
@@ -43,9 +39,9 @@ public class PostAdapter extends ArrayAdapter {
                     getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.item_post, null);
             viewHolder = new ViewHolder((ImageView) convertView.findViewById(R.id.postImageView),
-                                        (TextView) convertView.findViewById(R.id.postCategoriesTextView),
+                                        (TextView) convertView.findViewById(R.id.postTitleTextView),
                                         (TextView) convertView.findViewById(R.id.postDateTextView),
-                                        (TextView) convertView.findViewById(R.id.postContentTextView),
+                                        (TextView) convertView.findViewById(R.id.postAuthorTextView),
                                         (TextView) convertView.findViewById(R.id.postCommentsCountTextView));
             convertView.setTag(viewHolder);
         } else {
@@ -54,29 +50,28 @@ public class PostAdapter extends ArrayAdapter {
         PostModel postModel = postLst.get(position);
         if (postModel != null){
             viewHolder.postImageView.setImageResource(postModel.getPostImageId());
-            viewHolder.postCategoryTextView.setText(postModel.getPostTitle());
+            viewHolder.postTitleTextView.setText(postModel.getPostTitle());
             String time = getContext().getString(R.string.time_of_post, postModel.getPostDate());
             viewHolder.postDateTextView.setText(time);
-            viewHolder.postContentTextView.setText(postModel.getPostAuthor());
+            viewHolder.postAuthorTextView.setText(postModel.getPostAuthor());
             String commentsCount = getContext().getString(R.string.comments_amounts, postModel.getPostCommentCount());
             viewHolder.postCommentsCountTextView.setText(commentsCount);
-            /*TODO title --- category?? cambiar donde corresponda Chequear que es cada campo */
-        }
+         }
         return convertView;
     }
     private class ViewHolder {
         public final ImageView postImageView;
-        public final TextView postCategoryTextView;
+        public final TextView postTitleTextView;
         public final TextView postDateTextView;
-        public final TextView postContentTextView;
+        public final TextView postAuthorTextView;
         public final TextView postCommentsCountTextView;
         public ViewHolder(ImageView postImageView, TextView postCategoryTextView,
                           TextView postDateTextView, TextView postContentTextView,
                           TextView postCommentsCountTextView){
             this.postImageView = postImageView;
-            this.postCategoryTextView = postCategoryTextView;
+            this.postTitleTextView = postCategoryTextView;
             this.postDateTextView = postDateTextView;
-            this.postContentTextView = postContentTextView;
+            this.postAuthorTextView = postContentTextView;
             this.postCommentsCountTextView = postCommentsCountTextView;
         }
     }
